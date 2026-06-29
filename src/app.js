5e80ed87a9403a3130ec0cb16fb7ce1439fc3e53
Alname94 <alname94@gmail.com>
2026-06-29 10:31:19 -0300

    feat: add cors middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const grupoRoutes = require('./routes/grupoRoutes');
 const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/grupos', grupoRoutes);
-app.use('/api/usuario', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/api/health', (req, res) => {
     res.json({ status: 'ok', message: 'API lista' });
